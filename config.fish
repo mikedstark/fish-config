a62f9828e7af3c39229c47fe8c4d8fd5d83eb90c
Michael Stark <mikedstark@gmail.com>
2022-05-08 23:46:36 -0500

    moved config.fish up a dir

--- config.fish
+++ config.fish
@@ -1,16 +1,19 @@
-# Fish Config
+#Fish-Config
+
+#Remove greeting
+set fish_greeting
+
 alias c="clear"
-alias rr="source ~/.config/fish/conf.d/config.fish"
+alias rr="source ~/.config/fish/config.fish"
 alias ff="cd ~/.config/fish/"
-alias aa="vim ~/.config/fish/conf.d/config.fish"
+alias aa="vim ~/.config/fish/config.fish"
 alias dev="cd ~/Developer"
 alias dd="cd ~/Desktop"
 
-# Python
+#Python
 alias python='python3'
 alias pip='python3 -m pip'
 
-# Git
+#Git
 alias gs="git status"
 alias ga="git add -u"
-
