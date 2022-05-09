42aad1d5b928c58e36f1afc04e4b39377c830c73
Michael Stark <mikedstark@gmail.com>
2022-05-09 02:01:49 -0500

    removed alias

--- config.fish
+++ config.fish
@@ -1,11 +1,9 @@
 #Fish-Config
 
-#set -g theme_powerline_fonts no
-#set -g theme_nerd_fonts yes
-
 #Remove greeting
 #set fish_greeting
 
+#Alias
 alias c="clear"
 alias rr="source ~/.config/fish/config.fish"
 alias ff="cd ~/.config/fish/"
