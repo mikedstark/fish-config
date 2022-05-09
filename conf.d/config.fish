78663d048bc8ef15c43899247abf525dff9f67d3
Michael Stark <mikedstark@gmail.com>
2022-05-08 22:37:15 -0500

    initial

---
+++ conf.d/config.fish
@@ -0,0 +1,17 @@
+# Fish Config
+
+alias c="clear"
+alias rr="source ~/.config/fish/conf.d/config.fish"
+alias ff="vim ~/.config/fish/conf.d/config.fish"
+alias aa="vim ~/.config/fish/conf.d/config.fish"
+alias dev="cd ~/Developer"
+alias dd="cd ~/Desktop"
+
+# Python
+alias python='python3'
+alias pip='python3 -m pip'
+
+# Git
+alias gs="git status"
+alias ga="git add -u"
+
